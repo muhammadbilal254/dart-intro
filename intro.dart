05df7ae375ce407995a52cef50b647cd86c548e3
Muhammad Bilal <muhammadbilal254542@gmail.com>
2022-01-27 01:06:16 +0500

    initialize

---
+++ intro.dart
@@ -0,0 +1,116 @@
+void main() {
+  
+//   print("Hello World!");
+  
+  
+//   Data Types in dart
+  /*
+   * The Dart language supports 
+   * the following types−
+      * Numbers
+      * Strings
+      * Booleans
+      * Lists
+      * Maps
+   */
+  
+//   * Numbers *
+//     Numbers in Dart are used to represent numeric literals 
+      
+//   Integers : for store Whole numbers. {int}
+  
+//   int number = 25;
+//   print(number);
+  
+// Double : for storing decimal values. {double}
+
+  //   double deci = 2.5;
+//   print(deci);
+  
+//   num a = 2.5;
+//   print(a);
+//   a = 4;
+//   print(a);
+  
+  
+//   * Strings *
+// Strings represent a sequence of characters.
+
+//   String myName = "Muhammad Bilal";
+//   print("name : " + myName);
+  
+
+//  * Boolean *
+// The Boolean data type represents Boolean values 
+// true and false
+  
+//   bool checkStatus = true;
+//   print(checkStatus);
+//   checkStatus = false;
+//   print(checkStatus);
+
+// * List And Map *
+//   The data types list and map are used 
+//   to represent a collection of objects.
+
+
+  
+//   * Variables *
+// A variable is “a named space in the memory”
+//   that stores values.
+  
+//   var name = 'Smith';
+//   print("$name");
+  
+  
+  
+  //        * LOOPS *
+//   for loops
+
+  // EX : 1
+  
+//   for (int i=0 ; i<10 ; i++){
+//     print("$i + Hello Dart & Flutter ");
+//   }
+  
+//   EX : 2
+//  * finding Factorial *
+
+  //   print("factorial")
+//   var num1 = 10;
+//   var factorial1 = 1;
+  
+//   for(int i = num1 ; i >= 1; i-- ){
+//     factorial1 *= i;
+//     print(i);
+//   }
+//     print(factorial1);
+  
+  
+  //   EX : 3
+//   * Fibonacci numbers *
+ 
+//   for(dynamic temp, i = 0, j = 1; j<40; temp = i, i = j, j = i + temp) { 
+//       print('${j}'); 
+//    }  
+  
+//   * For in loop *
+  
+//    var obj = [12,13,14]; 
+   
+//    for (var x in obj) { 
+//       print(x); 
+// } 
+  
+
+//   number properties
+  
+//   var str = num.parse('2');
+//   print(str);
+  
+  var n = "g"; 
+   print(n.hashCode); 
+  
+  
+}
+
